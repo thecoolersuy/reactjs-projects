16545cb13b2486710aa12723cefd058c909c4cd5
oatkrunch <suyalsahukhala787@gmail.com>
2026-03-31 20:48:11 +0545

    feat(redux): added required configuration for redux : store,reducer,useSelector, useDispatcher

---
+++ reduxTodo/reduxTodo/src/App.jsx
@@ -0,0 +1,17 @@
+import TodoForm from "./components/TodoForm"
+import TodoItem from "./components/TodoItem"
+
+function App() {
+
+
+  return (
+    <>
+      <h1> Redux Toolkit</h1>
+      <TodoForm />
+      <TodoItem />
+      
+    </>
+  )
+}
+
+export default App
